28b9b3ac68cff68bef55792f45d3a0a2ee9cf3fc
zkdot <zkdots1@gmail.com>
2025-12-27 22:17:43 +0100

    removed linking for theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,42 @@
-/home/zk/.config/omarchy/current/theme/neovim.lua
+return {
+
+  {
+
+    "wnkz/monoglow.nvim",
+
+    lazy = false,
+
+    priority = 1000,
+
+    config = function()
+      require("monoglow").setup({
+
+        on_colors = function(_) end,
+
+        on_highlights = function(h, c)
+          -- Make all function tokens use the glow accent
+
+          h.Function = { fg = c.glow, bold = true }
+
+          h["@function"] = { fg = c.glow, bold = true }
+
+          h["@function.call"] = { fg = c.glow }
+
+          h["@method"] = { fg = c.glow }
+
+          h["@method.call"] = { fg = c.glow }
+
+          h.String = { fg = "#6EE0B4" }
+
+          h["@string"] = { fg = "#6EE0B4" }
+
+          h["@variable.builtin"] = { fg = c.glow, bold = true }
+
+          h.LspInlayHint = { fg = "#606060", italic = true }
+        end,
+      })
+
+      vim.cmd.colorscheme("monoglow")
+    end,
+  },
+}
